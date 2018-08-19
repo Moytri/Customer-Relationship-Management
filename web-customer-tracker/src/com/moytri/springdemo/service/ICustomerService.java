7f46a11baf9b7f4397bee747917af5510ec0992c
moytri <moytritopa@gmail.com>
2018-08-19 16:10:59 -0700

    n/a

--- web-customer-tracker/src/com/moytri/springdemo/service/ICustomerService.java
+++ web-customer-tracker/src/com/moytri/springdemo/service/ICustomerService.java
@@ -7,4 +7,5 @@ import com.moytri.springdemo.entity.Customer;
 public interface ICustomerService {
 	public List<Customer> getCustomers();
 	public void saveCustomer(Customer customer);
+	public Customer getCustomers(int id);
 }
